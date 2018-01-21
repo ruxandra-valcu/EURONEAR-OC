4dedb8e84ef7319b75c1e70137cf1fb245188e2a
Eugen Croitoru <eugennc@gmail.com>
2018-01-21 17:27:27 +0200

    Old interface

---
+++ omc.php
@@ -0,0 +1,12 @@
+<?php 
+require_once("tools-header.php"); 
+toolHeader("OMinC Tool");
+
+require_once("config.php");
+require_once("wikiplugin_omc.php");
+echo wikiplugin_omc("","");
+
+toolFooter("");
+
+require_once("tools-footer.php"); ?>
+
